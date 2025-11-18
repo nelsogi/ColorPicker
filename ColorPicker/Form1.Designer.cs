c39549d68e2fbb92173f02c7c4cb93337df1675d
Nelson Rossi <nelson.rss@eduge.ch>
2025-11-18 15:36:09 +0100

    Ajoutez des fichiers projet.

---
+++ ColorPicker/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ColorPicker
+{
+    partial class frmColorPicker
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmColorPicker));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pnlTitleBar = new System.Windows.Forms.Panel();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.ntfiContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitAppToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlTitleBar.SuspendLayout();
+            this.ntfiContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.ContextMenuStrip = this.ntfiContextMenu;
+            resources.ApplyResources(this.notifyIcon, "notifyIcon");
+            // 
+            // pnlTitleBar
+            // 
+            this.pnlTitleBar.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pnlTitleBar.Controls.Add(this.btnHide);
+            resources.ApplyResources(this.pnlTitleBar, "pnlTitleBar");
+            this.pnlTitleBar.Name = "pnlTitleBar";
+            // 
+            // btnHide
+            // 
+            resources.ApplyResources(this.btnHide, "btnHide");
+            this.btnHide.FlatAppearance.BorderSize = 0;
+            this.btnHide.Name = "btnHide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // ntfiContextMenu
+            // 
+            this.ntfiContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.quitAppToolStripMenuItem});
+            this.ntfiContextMenu.Name = "ntfiContextMenu";
+            resources.ApplyResources(this.ntfiContextMenu, "ntfiContextMenu");
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            resources.ApplyResources(this.openToolStripMenuItem, "openToolStripMenuItem");
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // quitAppToolStripMenuItem
+            // 
+            this.quitAppToolStripMenuItem.Name = "quitAppToolStripMenuItem";
+            resources.ApplyResources(this.quitAppToolStripMenuItem, "quitAppToolStripMenuItem");
+            this.quitAppToolStripMenuItem.Click += new System.EventHandler(this.quitAppToolStripMenuItem_Click);
+            // 
+            // frmColorPicker
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Controls.Add(this.pnlTitleBar);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "frmColorPicker";
+            this.ShowIcon = false;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.pnlTitleBar.ResumeLayout(false);
+            this.ntfiContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Panel pnlTitleBar;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.ContextMenuStrip ntfiContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitAppToolStripMenuItem;
+    }
+}
+
---
+++ ColorPicker/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorPicker
+{
+    public partial class frmColorPicker : Form
+    {
+        public frmColorPicker()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            notifyIcon.Visible = true;
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            notifyIcon.Visible = true;
+        }
+
+        private void quitAppToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ntfiContextMenu_Click(object sender, EventArgs e)
+        {
+            ntfiContextMenu.Show();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+    }
+}
